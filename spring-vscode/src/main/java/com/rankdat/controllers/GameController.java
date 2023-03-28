d20bf35a4a485ed46bdb9653e1ca640a88e79702
hitoshidevx <g.hitoshi.yokogawa@gmail.com>
2023-03-27 21:49:01 -0300

    Projeto Spring Framework Criado com Sucesso

---
+++ spring-vscode/src/main/java/com/rankdat/controllers/GameController.java
@@ -0,0 +1,28 @@
+package com.rankdat.controllers;
+
+import java.math.BigDecimal;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rankdat.models.Game;
+import java.time.LocalDate;
+
+@RestController
+public class GameController {
+
+    @GetMapping("/api/jogos")
+    public Game show(){
+        var jogo = new Game(
+                    new BigDecimal(100), 
+                    LocalDate.now(), 
+                    "cinema",
+                    "shutup"
+                );
+
+        return jogo;
+    }
+
+
+
+}
---
+++ spring-vscode/src/main/java/com/rankdat/models/Game.java
@@ -0,0 +1,55 @@
+package com.rankdat.models;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Game {
+    private BigDecimal valorJogo;
+    private LocalDate dataLancamento;
+    private String descricaoJogo;
+    private String nomeJogo;
+
+    public Game(BigDecimal valorJogo, LocalDate dataLancamento, String descricaoJogo, String nomeJogo) {
+        this.valorJogo = valorJogo;
+        this.dataLancamento = dataLancamento;
+        this.descricaoJogo = descricaoJogo;
+        this.nomeJogo = nomeJogo;
+    }
+
+    public BigDecimal getValorJogo() {
+        return valorJogo;
+    }
+
+    public void setValorJogo(BigDecimal valorJogo) {
+        this.valorJogo = valorJogo;
+    }
+
+    public LocalDate getDataLancamento() {
+        return dataLancamento;
+    }
+
+    public void setDataLancamento(LocalDate dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+
+    public String getDescricaoJogo() {
+        return descricaoJogo;
+    }
+
+    public void setDescricaoJogo(String descricaoJogo) {
+        this.descricaoJogo = descricaoJogo;
+    }
+
+    public String getNomeJogo() {
+        return nomeJogo;
+    }
+
+    public void setNomeJogo(String nomeJogo) {
+        this.nomeJogo = nomeJogo;
+    }
+
+    @Override
+    public String toString() {
+        return "Game [valorJogo=" + valorJogo + ", dataLancamento=" + dataLancamento + ", descricaoJogo=" + descricaoJogo + ", nomeJogo=" + nomeJogo + "]";
+    }
+
+}
